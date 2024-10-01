0ddf432f329d171677bd0f7d23be9482035ce47a
Shounak Lohokare <shounaklohokare@gmail.com>
2024-10-01 22:10:50 +0530

    Addded home route to display F1 constructors' information

--- main.go
+++ main.go
@@ -1,22 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"path"
+	"text/template"
 )
+
+type Constructor struct {
+	Name string `json:"team_name"`
+	Base string `json:"base"`
+}
+
+type Constructors []Constructor
 
 func main() {
 
 	fmt.Println("Starting server on port 8080")
 
-	http.HandleFunc("/", handler)
+	var constructors Constructors
+
+	err := parseFile(&constructors)
+	if err != nil {
+		fmt.Printf("Error occured :- %v", err)
+		return
+	}
+
+	http.HandleFunc("/", constructors.handler)
+	http.HandleFunc("/{constructor_name}", constructors.contructorDetailsPageHandler)
 
 	http.ListenAndServe(":8080", nil)
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (constructors Constructors) handler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "Building a formula 1 App!")
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
 
+	tmpl.Execute(w, constructors)
+
+}
+
+func (constructors Constructors) contructorDetailsPageHandler(w http.ResponseWriter, r *http.Request) {
+
+	path := path.Base(r.URL.Path)
+
+	fmt.Fprintln(w, path)
+
+}
+
+func parseFile(contructors *Constructors) error {
+
+	f, err := os.Open("f1_constructors_basic_info.json")
+	if err != nil {
+		return err
+	}
+
+	err = json.NewDecoder(f).Decode(contructors)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
